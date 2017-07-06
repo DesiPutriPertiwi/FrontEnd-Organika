02475aac2a413d768187f7ca55970d5ed4703b08
DesiPutriPertiwi <desiputripertiwi@gmail.com>
2017-07-06 14:21:35 +0700

    profil setengah jadi

--- app/src/pages/profil/profil.ts
+++ app/src/pages/profil/profil.ts
@@ -1,26 +1,26 @@
 import { Component } from '@angular/core';
-import { ViewController, ActionSheetController, NavController, AlertController, NavParams,App, PopoverController } from 'ionic-angular';
+import { ViewController, ActionSheetController, LoadingController, NavController, ToastController, AlertController, NavParams,App, PopoverController } from 'ionic-angular';
 import { Auth } from '../../providers/auth';
 import { LoginPage } from '../login/login';
-//import { ProfileEditPage } from '../profile-edit/profile-edit';
+//import { EditProfilPage } from '../edit-profil/edit-profil';
 import { Storage } from '@ionic/storage';
 import { EditKeluarPage } from '../edit-keluar/edit-keluar';
 import { Camera} from '@ionic-native/camera';
-/*
-  Generated class for the Profil page.
+import { NgForm } from '@angular/forms';
+//import { AuthHttp } from 'angular2-jwt';
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-profil',
   templateUrl: 'profil.html'
 })
 export class ProfilPage {
-	nama: string;
+	user: {user_id?: string, username?: string, name?: string, email?: string, picture?: string, password?: string,  role?: string} = {};
+  nama : string;
+  base64Image: string;
   profilePict: string;
   picture: string;
   submitted = false;
+  loading : any;
 
 
   constructor(
@@ -32,6 +32,9 @@ export class ProfilPage {
   	public auth: Auth,
     public actionSheetCtrl: ActionSheetController,
     public camera: Camera,
+    public toastCtrl: ToastController,
+  	public loadCtrl: LoadingController,
+  //  public authHttp: AuthHttp,
     public popoverCtrl: PopoverController) {
 
   }
@@ -39,6 +42,15 @@ export class ProfilPage {
   ionViewWillEnter(){
     this.getName();
     this.getProfilePict();
+    this.auth.getData().then((value)=>{
+      this.user.username = value.username;
+      this.user.name = value.name;
+      this.user.email = value.email;
+      this.user.user_id = value.user_id;
+      this.user.picture = value.picture;
+      this.user.role = value.role;
+    });
+
   }
   presentPopover(event: Event) {
     let popover = this.popoverCtrl.create(EditKeluarPage);
@@ -91,6 +103,7 @@ editFoto() {
     	}, (err) => {
     });
   }
+
   getPhotoFromGallery(){
     this.camera.getPicture({
         destinationType: this.camera.DestinationType.DATA_URL,
@@ -102,11 +115,75 @@ editFoto() {
     	}, (err) => {
     });
   }
-
-  logout() {
-    this.auth.logout();
-    this.app.getRootNav().setRoot(LoginPage);
-    this.viewCtrl.dismiss();
+  
+  postUpdatePicture(){
+    this.loading = this.loadCtrl.create({
+        content: 'Uploading image...'
+    });
+    this.loading.present();
+    let param = JSON.stringify({
+      picture: this.base64Image
+    });
+/*    this.authHttp.post(this.auth.BASE_URL+'user/uploadPhoto',param).subscribe(res => {
+      this.loading.dismiss();
+      let response = res.json();
+      if(response.status==200) {
+        this.auth.updateProfilePict(response.picture);
+        this.user.picture = response.picture;
+        this.showAlert("Berhasil mengubah foto profile"); 
+      }         
+    }, err => { 
+        this.loading.dismiss();
+        this.showError(err);
+    });
+    */
   }
 
+  editProfil(){
+   // this.navCtrl.push(EditProfilPage);
+  }  
+
+  onUpdate(form: NgForm) {
+    this.submitted = true;
+    let loading = this.loadCtrl.create({
+        content: 'Tunggu sebentar...'
+    });
+    if (form.valid) {
+    	loading.present();
+      let param = JSON.stringify({
+     	  user_id : this.user.user_id,
+        username : this.user.username,
+        name : this.user.name,
+        email : this.user.email,
+        password: this.user.password
+      });
+  /*    this.authHttp.post(this.auth.BASE_URL+'user/update',param).subscribe(res => {
+      	loading.dismiss();
+        let response = res.json();
+        if(response.status == 200) {
+          this.auth.login(response.data);
+          this.showAlert(response.message);
+          this.navCtrl.popToRoot();
+        } else if(response.status == 400) {
+          this.showAlert(response.message);
+        }
+      }, err => { 
+      	loading.dismiss();
+         this.showError(err);
+      });
+*/
+    }
+  }
+  showError(err: any){  
+    err.status==0? 
+    this.showAlert("Tidak ada koneksi. Cek kembali sambungan Internet perangkat Anda"):
+    this.showAlert("Tidak dapat menyambungkan ke server. Mohon muat kembali halaman ini");
+  }
+  showAlert(message: string){
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000
+    });
+    toast.present();
+  }
 }
